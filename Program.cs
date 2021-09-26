e13821900dd07299989e462a2c88e8dc2f45e38d
samuel <samuelreyesiglesias@gmail.com>
2021-09-25 17:32:57 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppExampleGenerics
+{
+    public interface INumeros
+    {
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+    }
+    public class Numeros : INumeros
+    {
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+    }
+    public class OperacionesMatematicas<T> where T : INumeros
+    {
+        
+        public static T Numeros { get; set; }
+        public static void SetNumeros(T Numeros_)
+        {
+            Numeros = Numeros_;
+        }
+        public static int Sumar()
+        {
+            return Numeros.N1 + Numeros.N2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Usando generics class.
+            //first we instance the class
+            Numeros N = new Numeros();
+            N.N1 = 1;
+            N.N2 = 2;
+           OperacionesMatematicas<Numeros>.SetNumeros(N);
+            int ResultadoSumar = OperacionesMatematicas<Numeros>.Sumar() ;
+            Console.WriteLine($"El resultado de la suma es:{ResultadoSumar}"); 
+            
+            //Primer Ejemplo de meotodo utilizando Generic.
+            MostrarMensaje<string>("Mi mensaje a mostrar");
+
+            //Segundo ejemplo de llaamda a metodo utilizando generic. en este caso no es necesario utilizar 
+            //o especificar el tipo de dat.. se realiza una asignacion implicita del tipo de dato enviado en los parametros del metodo al invocarlo.
+            int Parametro1=100;
+            int Parametro2=333;
+            int ValorSuma = Sumatoria(Parametro1, Parametro2);
+            Console.WriteLine("El valor de la sumatoria de un metodo utilizando generics es:" + ValorSuma);
+            
+        }
+
+        public static void MostrarMensaje<T>(T MiMensajeAMostrar){
+            if (MiMensajeAMostrar.GetType() == typeof(string)) { 
+                Console.WriteLine("Mi mensaje a Mostrar es:"+MiMensajeAMostrar);
+            }else{
+                Console.WriteLine("Tipo de dato no soportado.-");
+            }
+        }
+
+        public static int Sumatoria<Type>(Type Numero1,Type Numero2)
+        {
+            if(Numero1.GetType()==typeof(int) && Numero2.GetType() == typeof(int))
+            {
+                try
+                {
+                    int n1 = Convert.ToInt32(Numero1);
+                    int n2 = Convert.ToInt32(Numero2);
+                    return n1 + n2;
+                }catch(Exception E)
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+
+}
---
+++ PropagationSNT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ConsoleAppExampleGenerics
+{
+    class PropagationSNT
+    {
+        //this class will be used to connnect to the objetive Server, PC, Router or device in the Network...
+        HttpClient SNTClient; 
+        protected string URLStringNeeded =null;
+        public PropagationSNT(string AssignURL)
+        {
+            URLStringNeeded = AssignURL;
+            SNTClient = new HttpClient();
+            SNTClient.BaseAddress = new Uri(URLStringNeeded);
+        }
+    }
+}
